47b0ac26f3e05d4a5536a139910506ca7d409fc0
James <jfnelson2000@gmail.com>
2023-02-15 20:26:41 -0800

    basic About Us layout made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import CarouselPage from './Components/Carousel/Carousel';
+import AboutUs from './Components/AboutUs/AboutUs';
 
 function App() {
   return (
