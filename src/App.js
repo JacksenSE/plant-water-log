bb08bf9a3d4cecc0334f8bcd0c09853d1309a784
Diego Velasquez <da.velasquezgomez@gmail.com>
2023-02-15 18:38:27 -0800

    Added Navbar, logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
+import NavbarComp from './components/NavbarComp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>House-Plant Watering Log</h1>
+        <NavbarComp/>
+        
     </div>
   );
 }
