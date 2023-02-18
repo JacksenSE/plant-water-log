cab2d85c21039df109f012d7e39875810225d5a1
James <jfnelson2000@gmail.com>
2023-02-18 11:21:46 -0800

    added nonfunctional login/signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import AboutUs from './Components/AboutUs/AboutUs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar } from 'react-bootstrap';
 import NavbarComp from './Components/Navbar/NavbarComp'
+import LogIn from './Components/LogIn/LogIn';
+import SignUp from './Components/SignUp/SignUp';
 
 function App() {
   return (
