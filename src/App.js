261dd5e098fc7b101270f68af656dc0749a3509c
Diego Velasquez <da.velasquezgomez@gmail.com>
2023-02-18 09:57:45 -0800

    Plant profiles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar } from 'react-bootstrap';
 import NavbarComp from './components/NavbarComp';
+import PlantProfiles from './components/PlantProfiles';
 import styled from 'styled-components';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
         <NavbarComp/>
         <h1 style={styles}>House-Plant Watering Log</h1>
+        <PlantProfiles/>
         </div>
   );
 }
