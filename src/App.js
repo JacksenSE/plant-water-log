31c2bace9cf2385abeba6cc4be25cdeee9a363a3
James <jfnelson2000@gmail.com>
2023-02-18 10:13:32 -0800

    App.js fixes and better looking About

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import CarouselPage from './Components/Carousel/Carousel';
 import AboutUs from './Components/AboutUs/AboutUs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar } from 'react-bootstrap';
-import NavbarComp from './components/Navbar/NavbarComp';
-import CarouselPage from './components/Carousel/Carousel';
+import NavbarComp from './Components/Navbar/NavbarComp'
 
 function App() {
   return (
     <div className="App">
       <CarouselPage/>
       <NavbarComp/>
+      <AboutUs />
     </div>
   );
 }
