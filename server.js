73a6d7bd34dbd1949efd823f3d5ff3d6a25ce4b6
JacksenSE <109568831+JacksenSE@users.noreply.github.com>
2023-02-18 22:22:21 -0800

    trying to get server to run

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ require('dotenv').config()
 
 //MIDDLEWARE
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'ui/build')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'ui/build', 'index.html'));
 })
+
 //get all plants
 app.get("/api/v1/plants", (req,res) => {
     console.log("route ran")
