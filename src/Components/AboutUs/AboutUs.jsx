8e45271850129a11f253223f2a22c466eaf02161
Diego Velasquez <da.velasquezgomez@gmail.com>
2023-02-21 22:27:52 -0800

    About Us Info

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -4,33 +4,31 @@ import './AboutUs.css';
 function AboutUs () {
     return (
         <div className="aboutContainer">
-            <h1>About ?appName?</h1>
+            <h1>About Plant Watering Log</h1>
             <div className="aboutChild">
+                
                 <div>
-                    <h3>ABOUT</h3>
+                    <h3>Inspiration</h3>
                     <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                        Ipsa eos dolorem quisquam iure harum rerum eius! Sit vitae 
-                        natus perspiciatis cum tempore? Ea sapiente quaerat 
-                        necessitatibus iusto, dolorem consequatur nisi?
+                       Its pretty hard making sure to water your plants consistently,
+                       specially when each one has a different watering schedule and requirements. 
+                       This log was made to fix this issue. 
                     </p>
                 </div>
                 <div>
-                    <h3>ABOUT</h3>
+                    <h3>Purpose</h3>
                     <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                        Ipsa eos dolorem quisquam iure harum rerum eius! Sit vitae 
-                        natus perspiciatis cum tempore? Ea sapiente quaerat 
-                        necessitatibus iusto, dolorem consequatur nisi?
+                        This log was made to solve the problem of keeping track of the 
+                        watering schedule of the different plants around your home by creating profiles for each one
+                        and manually adding important and helpful information for each one. 
                     </p>
                 </div>
                 <div>
-                    <h3>ABOUT</h3>
+                    <h3>Use</h3>
                     <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                        Ipsa eos dolorem quisquam iure harum rerum eius! Sit vitae 
-                        natus perspiciatis cum tempore? Ea sapiente quaerat 
-                        necessitatibus iusto, dolorem consequatur nisi?
+                        After you've created a profile for each plant, you'll be able to
+                         keep track of the last watering time, have a small description and even add
+                         a reminder of the sunlight requirement for each. 
                     </p>
                 </div>
             </div>
