e9b48ffeacf3643be6886de362129b52687fbb00
Diego Velasquez <diegovelasquez@Diegos-iMac.local>
2023-02-22 16:24:42 -0800

    added logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from './Components/Home';
 import Create from './Components/Create';
 import Update from './Components/Update';
+import plantLogo from './Components/plant-logo.jpg';
 
 
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <BrowserRouter>
     <nav>
+      <img src={plantLogo} height="100"/>
       <h1>Plant Water Log</h1>
       <Link to="/">Home</Link>
       <Link to="/create">Create New Plant</Link>
